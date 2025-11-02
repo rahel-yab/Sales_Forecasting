8781beca86594b010eec39cee6eecf20ee2e0b90
rahel yabebal <rahelyabebal@gmail.com>
2025-11-02 15:43:25 +0300

    finalized model training

--- sales_forecasting.py
+++ sales_forecasting.py
@@ -15,7 +15,7 @@ df["date"] = pd.to_datetime(df["date"])
 
 df.sort_values("date" , inplace=True)
 df.reset_index(inplace=True , drop = True)
-print(df.head())
+df.head()
 
 train = df[df["date"] < "2017-01-01"]
 test = df[df["date"] >= "2017-01-01"]
@@ -45,3 +45,17 @@ enricher.calculate_metrics(train_features , train_target ,
                            estimator=model,
                            scoring="mean_absolute_percentage_error"
                            )
+
+enriched_train_features = enricher.transform(train_features , keep_input=True)
+enriched_test_features = enricher.transform(test_features , keep_input=True)
+enriched_train_features.head()
+
+# prediction with the original data set we have
+model.fit(train_features , train_target)
+preds = model.predict(test_features)
+eval_metric(test_target.values, preds , "SMAPE")
+
+# prediction with the enriched data set from upgini
+model.fit(enriched_train_features , train_target)
+enriched_preds = model.predict(enriched_test_features)
+eval_metric(test_target.values, enriched_preds , "SMAPE")
